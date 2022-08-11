f91d993d686fdf23d2b5bc8b4f6896130331cfbf
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-08-11 17:51:33 +0100

    first commit

---
+++ pages/about.jsx
@@ -0,0 +1,13 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        App to find out Sport news like Football, F1, Tennis, Cricket, Boxing,
+        Golf etc
+      </p>
+    </div>
+  );
+};
+
+export default About;
