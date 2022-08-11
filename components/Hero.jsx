a6da3755cfe3c2bcebba7b401d1c721f1ffaa4a3
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-08-11 17:53:21 +0100

    first commit

---
+++ components/Hero.jsx
@@ -0,0 +1,11 @@
+import styles from '@/styles/Hero.module.css';
+
+const Hero = () => {
+  return (
+    <div className={styles.hero}>
+      <h1>Find Out the Latest Sport News</h1>
+    </div>
+  );
+};
+
+export default Hero;
