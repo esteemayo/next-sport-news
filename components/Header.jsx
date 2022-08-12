07a45e22b97b4a1efd69674e5c7b017beb2eb325
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-08-12 02:06:36 +0100

    handled logout button and user authentication

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,27 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import styles from '@/styles/Header.module.css';
+import { setLogout } from '@/features/auth/authSlice';
 
 const Header = () => {
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => ({ ...state.auth }));
+
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const handleLogout = () => {
+    dispatch(setLogout());
+    router.push('/');
+  };
+
+  useEffect(() => {
+    setCurrentUser(user);
+  }, [user]);
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
@@ -16,21 +36,31 @@ const Header = () => {
               News
             </Link>
           </li>
-          <li>
-            <Link href='/news/add' passHref>
-              Add News
-            </Link>
-          </li>
-          <li>
-            <Link href='/auth/dashboard' passHref>
-              Dashboard
-            </Link>
-          </li>
-          <li>
-            <Link href='/auth/login'>
-              <a className='btn-secondary'>Login</a>
-            </Link>
-          </li>
+          {currentUser ? (
+            <>
+              <li>
+                <Link href='/news/add' passHref>
+                  Add News
+                </Link>
+              </li>
+              <li>
+                <Link href='/auth/dashboard' passHref>
+                  Dashboard
+                </Link>
+              </li>
+              <button className='btn-secondary' onClick={handleLogout}>
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link href='/auth/login'>
+                  <a className='btn-secondary'>Login</a>
+                </Link>
+              </li>
+            </>
+          )}
           <li>
             <Link href='/about' passHref>
               About
