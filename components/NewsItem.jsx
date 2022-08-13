76a5f51552f6c8a29686ad7115902fc70f9ca455
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-08-13 16:38:49 +0100

    first commit

---
+++ components/NewsItem.jsx
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import Moment from 'react-moment';
+import { FaUser } from 'react-icons/fa';
+
+import styles from '@/styles/NewsItem.module.css';
+
+const NewsItem = ({ name, slug, date, time, user, image }) => {
+  return (
+    <div className={styles.news}>
+      <div className={styles.img}>
+        <Image
+          src={image ? image : '/images/hero.jpg'}
+          width={150}
+          height={100}
+          alt={name}
+        />
+      </div>
+      <div className={styles.info}>
+        <span>
+          <Moment format='yyyy-MM-DD'>{date}</Moment> {time}
+        </span>
+        <h3>{name}</h3>
+        <p>
+          <FaUser /> Posted By: <strong>{user.username}</strong>
+        </p>
+      </div>
+      <div className={styles.link}>
+        <Link href={`/news/${slug}`}>
+          <a className='btn'>Read More</a>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NewsItem;
