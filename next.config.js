d6a1f7219249358e8b93c2ded9a344afc592d6e3
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-08-13 02:25:06 +0100

    first commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['res.cloudinary.com']
+  },
+  env: {
+    NEXT_PUBLIC_DEV_API_URL: process.env.NEXT_PUBLIC_DEV_API_URL,
+    NEXT_PUBLIC_PROD_API_URL: process.env.NEXT_PUBLIC_PROD_API_URL
+  }
+}
+
+module.exports = nextConfig
