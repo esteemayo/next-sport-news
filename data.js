7683d26207741d2f324b5daa4175278a0e5137ef
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-08-13 02:22:23 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,26 @@
+export const addInputs = [
+  {
+    id: 'name',
+    type: 'text',
+    name: 'name',
+    label: 'Name',
+  },
+  {
+    id: 'date',
+    type: 'date',
+    name: 'date',
+    label: 'Date',
+  },
+  {
+    id: 'time',
+    type: 'time',
+    name: 'time',
+    label: 'Time',
+  },
+  {
+    id: 'file',
+    type: 'file',
+    name: 'file',
+    label: 'Image',
+  },
+];
