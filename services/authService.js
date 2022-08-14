dbe5e95746e0fb2611249b8c1f36b90a6d6b12da
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-08-14 03:01:50 +0100

    added new routes

--- services/authService.js
+++ services/authService.js
@@ -1,10 +1,15 @@
+import axios from 'axios';
 import http from './httpService';
 
 const apiEndpoint = '/auth';
 const tokenKey = 'accessToken';
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 export const login = (credentials) =>
   http.post(`${apiEndpoint}/login`, credentials);
+
+export const nextLogin = (credentials) =>
+  axios.post(`${apiUrl}/auth/login`, credentials);
 
 export const getJwt = () => {
   if (typeof window !== 'undefined') {
