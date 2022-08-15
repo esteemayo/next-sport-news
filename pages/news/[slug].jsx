54c8fffab68071d64350e8dabc1ba19c3db8aa32
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-08-15 15:41:19 +0100

    implemented disqus comment

--- pages/news/[slug].jsx
+++ pages/news/[slug].jsx
@@ -5,31 +5,43 @@ import { FaUser } from 'react-icons/fa';
 
 import styles from '@/styles/News.module.css';
 import * as sportAPI from '@/services/sportService';
+import DisqusThread from '@/components/DisqusThread';
 
 const SingleNews = ({ news }) => {
+  const showDisqusComment = () => {
+    return (
+      <>
+        <DisqusThread id={news._id} title={news.name} path={news.slug} />
+      </>
+    );
+  };
+
   return (
-    <div className={styles.news}>
-      <Link href='/news'>
-        <a className={styles.back}>Go Back</a>
-      </Link>
-      <br />
-      <span>
-        <Moment format='yyyy-MM-DD'>{news.date}</Moment> {news.time}
-      </span>
-      <p>
-        <FaUser /> Posted By: <strong>{news.user.username}</strong>
-      </p>
-      <h1>{news.name}</h1>
-      <div className={styles.image}>
-        <Image
-          src={news.image ? news.image : '/images/hero.jpg'}
-          width={900}
-          height={600}
-          alt={news.name}
-        />
+    <>
+      <div className={styles.news}>
+        <Link href='/news'>
+          <a className={styles.back}>Go Back</a>
+        </Link>
+        <br />
+        <span>
+          <Moment format='yyyy-MM-DD'>{news.date}</Moment> {news.time}
+        </span>
+        <p>
+          <FaUser /> Posted By: <strong>{news.user.username}</strong>
+        </p>
+        <h1>{news.name}</h1>
+        <div className={styles.image}>
+          <Image
+            src={news.image ? news.image : '/images/hero.jpg'}
+            width={900}
+            height={600}
+            alt={news.name}
+          />
+        </div>
+        <p>{news.detail}</p>
       </div>
-      <p>{news.detail}</p>
-    </div>
+      {showDisqusComment()}
+    </>
   );
 };
 
